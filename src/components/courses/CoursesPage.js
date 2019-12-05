86620368ad5d22d354eb4df43b5ef8e7f0455dac
Witecki,Amanda <Amanda.Witecki@Cerner.com>
2019-12-05 09:35:36 -0500

    Initial commit

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+
+class CoursesPage extends React.Component {
+
+  componentDidMount() {
+    this.props.actions.loadCourses().catch(error => alert("Loading courses failed" + error));
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Courses</h2>
+
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+
+      </>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  }
+}
+
+CoursesPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
